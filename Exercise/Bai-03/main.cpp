6c84a61ef9ebd99754b31e191e7ec7511c1985b4
hieu313 <hieunm3103@gmail.com>
2024-06-17 09:44:46 +0700

    operations with fractions

--- Exercise/Bai-03/main.cpp
+++ Exercise/Bai-03/main.cpp
@@ -3,11 +3,15 @@
 #include <math.h>
 
 using namespace std;
-long long GCD(long long a, long long b)
+long long UCLN(long long a, long long b)
 //Greatest Common Divisor: uoc chung lon nhat
 {
     if (b==0) return a;
-    return GCD(b, a%b);
+    return UCLN(b, a%b);
+}
+long long BCNN(long long a, long long b)
+{
+    return (a/UCLN(a, b))*b;
 }
 class PhanSo
 {
@@ -17,9 +21,13 @@ class PhanSo
         PhanSo(long long tu, long long mau);
         friend istream& operator >> (istream &in, PhanSo &p);
         friend ostream& operator << (ostream &out, PhanSo p);
+        friend PhanSo operator + (PhanSo a, PhanSo b);
+        friend PhanSo operator - (PhanSo a, PhanSo b);
+        friend PhanSo operator * (PhanSo a, PhanSo b);
+        friend PhanSo operator / (PhanSo a, PhanSo b);
         void rutgon();
 };
-PhanSo::PhanSo(long long tu, long long mau)
+PhanSo::PhanSo(long long tu=1, long long mau=1)
 {
     this->tu = tu;
     this->mau = mau;
@@ -32,22 +40,56 @@ istream& operator >> (istream& in, PhanSo& p)
 }
 ostream& operator << (ostream& out, PhanSo p)
 {
-    out << p.tu << "/" << p.mau;
+    out << p.tu << "/" << p.mau<<endl;
     return out;
 }
-
+PhanSo operator+(PhanSo a, PhanSo b)
+{
+    PhanSo tong;
+    long long mau_chung = BCNN(fabs(a.mau), fabs(b.mau));
+    tong.tu = a.tu * (mau_chung/a.mau) + b.tu * (mau_chung / b.mau);
+    tong.mau = mau_chung;
+    tong.rutgon();
+    return tong;
+}
+PhanSo operator-(PhanSo a, PhanSo b)
+{
+    PhanSo hieu;
+    long long mau_chung = BCNN(fabs(a.mau), fabs(b.mau));
+    hieu.tu = a.tu * (mau_chung/a.mau) - b.tu * (mau_chung / b.mau);
+    hieu.mau = mau_chung;
+    hieu.rutgon();
+    return hieu;
+}
+PhanSo operator*(PhanSo a, PhanSo b)
+{
+    PhanSo tich(a.tu*b.tu, a.mau*b.mau);
+    tich.rutgon();
+    return tich;
+}
+PhanSo operator/(PhanSo a, PhanSo b)
+{
+    PhanSo thuong(a.tu*b.mau, a.mau*b.tu);
+    thuong.rutgon();
+    return thuong;
+}
 void PhanSo::rutgon()
 {
-
-    long long g = GCD(fabs(this->tu), fabs(this->mau));
+    long long g = UCLN(fabs(this->tu), fabs(this->mau));
     this->tu /= g;
     this->mau /= g;
 }
 int main()
 {
-    PhanSo p(1, 1);
+    PhanSo p;
     cin>>p;
     p.rutgon();
     cout<<p;
+    PhanSo q;
+    cin>>q;
+    cout << p+q;
+    cout << p-q;
+    cout << p*q;
+    cout << p/q;
     return 0;
 }
